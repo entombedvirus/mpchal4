d4699698e1a3197e45fa754695c3f68f685b8a41
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-01-02 04:20:01 +0000

    abandoned iouring

--- src/iodirect.rs
+++ src/iodirect.rs
@@ -1,15 +1,20 @@
 use std::{
+    collections::{LinkedList, VecDeque},
     fs,
     io::{self, Cursor, Write},
+    rc::Rc,
     sync::mpsc,
 };
 
+use rustix::fs::{FileExt, OpenOptionsExt};
+
 const CHUNK_SIZE: usize = 1 << 20;
+const ALIGN: usize = 4096;
 
 type Buf = Cursor<Vec<u8>>;
 
 fn new_buf() -> Buf {
-    let layout = std::alloc::Layout::from_size_align(CHUNK_SIZE, 4096).unwrap();
+    let layout = std::alloc::Layout::from_size_align(CHUNK_SIZE, ALIGN).unwrap();
     let vec = unsafe {
         let ptr = std::alloc::alloc_zeroed(layout);
         std::vec::Vec::from_raw_parts(ptr, 0, CHUNK_SIZE)
@@ -29,7 +34,7 @@ impl File {
             .write(true)
             .create(true)
             .truncate(true)
-            // .custom_flags(libc::O_DIRECT)
+            .custom_flags(libc::O_DIRECT)
             .open(path)
             .expect("failed to create result.txt");
         rustix::fs::fallocate(
@@ -44,18 +49,35 @@ impl File {
         let io_chan: Option<mpsc::Sender<Buf>> = Some(send.clone());
 
         let worker = std::thread::spawn(move || {
-            let ring = rio::new().expect("io_uring failed");
             let mut off = 0_usize;
+            let mut padn = 0_usize;
             for buf in recv {
                 let buf_len = buf.position() as usize;
-                let buf = buf.into_inner();
-                let wr = ring
-                    .write_at(&inner, &&buf[..buf_len], off as u64)
-                    .wait()
-                    .expect("write_at failed");
-                assert_eq!(wr, buf_len, "short write: wrote {wr} / {buf_len}");
-                off += buf_len;
+                let mut buf = buf.into_inner();
+                if buf_len % ALIGN != 0 {
+                    // this happens on the very last write
+                    assert_eq!(
+                        padn, 0,
+                        "non-aligned write is only expected once at the very end"
+                    );
+                    padn = ALIGN - buf_len % ALIGN;
+                    assert!(
+                        buf.len() + padn <= buf.capacity(),
+                        "buf will resize, which will destroy alignment guarantees"
+                    );
+                    for _ in 0..padn {
+                        buf.push(0_u8);
+                    }
+                }
+                inner
+                    .write_all_at(&buf, off as u64)
+                    .expect("worker: write_all_at failed");
+                off += buf.len();
             }
+
+            // truncate file to expected size since we might've
+            // written padding zero bytes for O_DIRECT alignment
+            rustix::fs::ftruncate(&inner, (off - padn) as u64).expect("ftruncate failed");
         });
 
         Self {
@@ -65,10 +87,17 @@ impl File {
         }
     }
 
-    pub fn write_bytes(&mut self, line: &[u8]) -> io::Result<()> {
+    pub fn write_bytes(&mut self, mut line: &[u8]) -> io::Result<()> {
         let buf = self.buf.get_ref();
         let cap = buf.capacity() - self.buf.position() as usize;
         if cap < line.len() {
+            let (partial, rem) = line.split_at(cap);
+            line = rem;
+            assert_eq!(
+                self.buf.write(partial).unwrap(),
+                partial.len(),
+                "write_bytes: partial: short write"
+            );
             self.flush().expect("write_bytes: flush failed");
         }
         assert_eq!(
