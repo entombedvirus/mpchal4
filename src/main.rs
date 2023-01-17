b526057268ff0049aabf81525b81bb43cb43fc33
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-01-17 04:28:46 +0000

    256bit works, but slow

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@
 #![feature(portable_simd)]
 #![feature(stdsimd_internal)]
 #![feature(stdsimd)]
-use std::{env, io};
+use std::{
+    arch::x86_64::{
+        _mm256_add_epi64, _mm256_cmplt_epu64_mask, _mm256_extracti128_si256, _mm256_load_si256,
+        _mm256_loadu_si256, _mm256_mask_mov_epi64, _mm256_min_epu64, _mm256_permute4x64_epi64,
+        _mm256_set1_epi64x, _mm256_set_epi64x, _mm256_setr_epi64x, _mm_cmplt_epu64_mask,
+        _mm_mask_mov_epi64,
+    },
+    env, io,
+    simd::{u64x2, u64x4, usizex4, Simd, SimdOrd},
+};
 
 use iodirect::{output_file::OutputFile, sorted_file::SortedFile, ALIGN, LINE_WIDTH_INCL_NEWLINE};
 
@@ -53,14 +62,22 @@ struct SortingWriter {
     input_files: Vec<SortedFile>,
     // a parallal vec to input_files that contains the top value from each SortedFile. Used to
     // speed up comparisons to find the min value.
-    min_values: Vec<u64>,
+    min_values: Vec<Simd<u64, { Self::LANES }>>,
 }
 
 impl SortingWriter {
+    const LANES: usize = 4;
+
     fn new(input_files: Vec<SortedFile>) -> Self {
         let min_values = input_files
-            .iter()
-            .map(|sf| *sf.peek().unwrap_or(&u64::MAX))
+            .chunks(Self::LANES)
+            .map(|sfs| {
+                let mut arr = [u64::MAX; Self::LANES];
+                for (i, sf) in sfs.iter().enumerate() {
+                    arr[i] = *sf.peek().unwrap_or(&u64::MAX);
+                }
+                Simd::from_array(arr)
+            })
             .collect();
         Self {
             input_files,
@@ -70,21 +87,103 @@ impl SortingWriter {
 
     fn write_to(&mut self, dest: &mut OutputFile) -> io::Result<()> {
         loop {
-            let Some(min_idx) = self.find_min_idx() else { return Ok(()) };
+            let Some(min_idx) = self.argmin2() else { return Ok(()) };
             let min_sf = &mut self.input_files[min_idx];
             let Some(line) = min_sf.peek_bytes() else { return Ok(()) };
             dest.write_bytes(line)?;
             min_sf.next();
-            self.min_values[min_idx] = *min_sf.peek().unwrap_or(&u64::MAX);
+            // self.min_values[min_idx] = *min_sf.peek().unwrap_or(&u64::MAX);
+            let new_val = *min_sf.peek().unwrap_or(&u64::MAX);
+            self.update_min_value_at(min_idx, new_val);
         }
     }
 
+    #[inline(never)]
     fn find_min_idx(&self) -> Option<usize> {
-        self.min_values
+        let min_values = self
+            .min_values
             .iter()
-            .enumerate()
-            .min_by_key(|(_, &val)| val)
-            .map(|(idx, _)| idx)
+            .fold(Simd::splat(u64::MAX), |acc, &x| acc.simd_min(x))
+            .to_array();
+        let min_value = min_values.into_iter().min().unwrap();
+        self.input_files
+            .iter()
+            .position(|sf| sf.peek() == Some(&min_value))
+    }
+
+    // See: https://en.algorithmica.org/hpc/algorithms/argmin/
+    fn argmin2(&self) -> Option<usize> {
+        unsafe {
+            let lane_width = _mm256_set1_epi64x(Self::LANES as i64);
+            let mut batch_start_addr = self.min_values.as_ptr() as *const _;
+
+            let mut min_values = _mm256_load_si256(batch_start_addr);
+            let mut indices = _mm256_setr_epi64x(0, 1, 2, 3);
+            batch_start_addr = batch_start_addr.add(1);
+
+            // find lane-wise min values and their indices
+            for _ in 1..self.min_values.len() {
+                let cur_values = _mm256_load_si256(batch_start_addr);
+                let cur_indices = _mm256_add_epi64(indices, lane_width);
+
+                let mask = _mm256_cmplt_epu64_mask(cur_values, min_values);
+                min_values = _mm256_mask_mov_epi64(min_values, mask, cur_values);
+                indices = _mm256_mask_mov_epi64(indices, mask, cur_indices);
+
+                batch_start_addr = batch_start_addr.add(1);
+            }
+
+            let min_values = u64x4::from(min_values);
+            let indices = usizex4::from(indices);
+            let min = *min_values.as_array().iter().min()?;
+            for idx in 0..Self::LANES {
+                if min_values[idx] == min {
+                    return Some(indices[idx]);
+                }
+            }
+            unreachable!("min will be found");
+            // indices
+            //     .as_array()
+            //     .iter()
+            //     .find(|idx| min_values[**idx] == *min)
+            //     .copied()
+
+            // // compare the top two mins to the bottom two in the 64x4 register
+            // let min_lo = _mm256_extracti128_si256(min_values, 0);
+            // let min_idx_lo = _mm256_extracti128_si256(indices, 0);
+            // let min_hi = _mm256_extracti128_si256(min_values, 1);
+            // let min_idx_hi = _mm256_extracti128_si256(indices, 1);
+
+            // let mask = _mm_cmplt_epu64_mask(min_lo, min_hi);
+            // let mins: u64x2 = _mm_mask_mov_epi64(min_hi, mask, min_lo).into();
+            // let min_idxs: u64x2 = _mm_mask_mov_epi64(min_idx_hi, mask, min_idx_lo).into();
+
+            // if mins[0] < mins[1] {
+            //     Some(min_idxs[0] as usize)
+            // } else {
+            //     Some(min_idxs[1] as usize)
+            // }
+        }
+    }
+
+    fn argmin(&self) -> Option<usize> {
+        let mut min_val = &u64::MAX;
+        let mut idx = 0;
+        for (batch_idx, batch) in self.min_values.iter().enumerate() {
+            let batch_min = batch.as_array().iter().min().unwrap();
+            if batch_min < min_val {
+                min_val = batch_min;
+                idx = batch_idx;
+            }
+        }
+        self.min_values.get(idx).map(|batch| {
+            let sub_idx = batch.as_array().iter().position(|x| x == min_val).unwrap();
+            idx * Self::LANES + sub_idx
+        })
+    }
+
+    fn update_min_value_at(&mut self, idx: usize, new_val: u64) {
+        self.min_values[idx / Self::LANES][idx % Self::LANES] = new_val;
     }
 }
 
